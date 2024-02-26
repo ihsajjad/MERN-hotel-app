76475a09f5b08ce1a6f893e339a75dec9b371a8e
Iftekher Hossen Sajjad <iftekherhossensajjad@gmail.com>
2024-02-26 15:27:59 +0600

    frontend: created login form and connected with the backend

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,4 +1,5 @@
 import { RegisterFormData } from "./pages/Register";
+import { SignInFormData } from "./pages/SignIn";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const register = async (fromData: RegisterFormData) => {
@@ -12,6 +13,19 @@ export const register = async (fromData: RegisterFormData) => {
   const responseBody = await response.json();
 
   if (!response.ok) throw new Error(responseBody.message);
+};
+
+export const signIn = async (FormData: SignInFormData) => {
+  const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
+    method: "POST",
+    credentials: "include",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(FormData),
+  });
+
+  const body = await response.json();
+  if (!response.ok) throw new Error(body.message);
+  return body;
 };
 
 export const validateToken = async () => {
