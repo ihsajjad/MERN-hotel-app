d8bd5c6d59794bcf1429b07b94c83d48b0c90d4c
ihsajjad <ihsajjad1@gmail.com>
2024-03-10 22:17:34 +0600

    frontend: implemented add hotel functionality

--- frontend/src/forms/ManageHotelForms/FacitiliesSection.tsx
+++ frontend/src/forms/ManageHotelForms/FacitiliesSection.tsx
@@ -13,7 +13,7 @@ const FacitiliesSection = () => {
       <h2 className="text-3xl font-bold mb-3">Facilities</h2>
       <div className="grid grid-cols-5 gap-3 ">
         {hotelFacilities.map((facility) => (
-          <label className="text-sm flex gap-1 text-gray-700">
+          <label className="text-sm flex gap-1 text-gray-700" key={facility}>
             <input
               type="checkbox"
               value={facility}
--- frontend/src/pages/AddHotel.tsx
+++ frontend/src/pages/AddHotel.tsx
@@ -1,7 +1,21 @@
+import { useMutation } from "react-query";
+import * as apiClient from "../api-client";
+import { useAppContext } from "../contexts/AppContext";
 import ManageHotelForms from "../forms/ManageHotelForms/ManageHotelForms";
 
 const AddHotel = () => {
-  return <ManageHotelForms />;
+  const { showToast } = useAppContext();
+
+  const { mutate, isLoading } = useMutation(apiClient.addMyHotel, {
+    onSuccess: () => showToast({ message: "Hotel saved!", type: "SUCCESS" }),
+    onError: () => showToast({ message: "Error saving hotel", type: "ERROR" }),
+  });
+
+  const handleSave = (hotelFormData: FormData) => {
+    console.log(hotelFormData);
+    mutate(hotelFormData);
+  };
+  return <ManageHotelForms onSave={handleSave} isLoading={isLoading} />;
 };
 
 export default AddHotel;
