e64f00710fa8a8b84e087617f20101b5ba30c488
ihsajjad <ihsajjad1@gmail.com>
2024-03-13 13:19:54 +0600

    created search bar and and get hotel api

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,8 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App.tsx";
 
 import { AppContextProvider } from "./contexts/AppContext.tsx";
+
+import { SearchContextProvider } from "./contexts/SearchContext.tsx";
 import "./index.css";
 
 const queryClient = new QueryClient({
@@ -18,7 +20,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AppContextProvider>
-        <App />
+        <SearchContextProvider>
+          <App />
+        </SearchContextProvider>
       </AppContextProvider>
     </QueryClientProvider>
   </React.StrictMode>
